e33186b07adfe091a4e9f2fbd8147d73336aaec0
programmerNizame <mdnizam12109@gmail.comm>
2022-03-01 02:08:01 +0600

    Update

--- js/app.js
+++ js/app.js
@@ -1,39 +1,79 @@
-const searchBtn = () =>{
-    const searchFiled = document.getElementById('search-filed');
-    const searchText = searchFiled.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displayPhone(data.data));
+const spanner = style =>{
+    document.getElementById('spanner').style.display = style
 }
-const displayPhone = phones =>{
+const searchBtn = () =>{
+    document.getElementById('display-result').textContent = ''
+    document.getElementById('display-details').textContent = ''
+    const searchFiled = document.getElementById('search-filed')
+    const searchResult = searchFiled.value
+    spanner('block')
+    searchFiled.value = ''
+    const error = document.getElementById('error')
+    if(searchResult == ''){
+        error.innerText = 'Enter a phones name'
+        error.className = 'text-danger'
+        spanner('none')
+    }
+    else{
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchResult}`
+            fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                if(data.data.length <= 0){
+                    error.innerText = `${data.data.length} search result found '${searchResult}'`
+                    error.className = 'text-danger'
+                    spanner('none')
+                }
+                else{
+                    error.innerText = `${data.data.length} result found '${searchResult}'`
+                    error.className = 'text-success'
+                    displaySearch(data.data)
+                    spanner('none')
+                }
+            })
+    }
+}
+const displaySearch = phones =>{
     console.log(phones)
-    const displayResult = document.getElementById('display-result');
+    const displayResult = document.getElementById('display-result')
     phones.forEach(phone => {
-        const newDiv = document.createElement('div');
-        newDiv.className = 'col'
-        newDiv.innerHTML = `
-            <div class="card shadow-sm h-100">
-                <img src="${phone.image}" class="card-img-top img-fluid" alt="...">
-                <div class="card-body text-center">
-                    <h5 class="card-title">${phone.phone_name}</h5>
-                    <h6>Brand: ${phone.brand}</h6>
-                    <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary text-white rounded-pill px-4">Phone Details</button>
-                </div>
-            </div>
+        const div = document.createElement('div')
+        div.className = 'col'
+        div.innerHTML = `
+        <div class="card shadow h-100">
+        <img src="${phone.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${phone.phone_name}</h5>
+          <h6 class="card-title">Brand: ${phone.brand}</h6>
+          <button onclick="displayDetails('${phone.slug}')" class="text-white btn btn-primary rounded-pill px-5">Details</button>
+        </div>
+      </div>
         `
-        displayResult.appendChild(newDiv)
+        displayResult.appendChild(div)
+        spanner('none')
     });
 }
-const phoneDetails = id =>{
+const displayDetails = id =>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`
     fetch(url)
     .then(res => res.json())
-    .then(data => showPhoneDetails(data))
+    .then(data => displayInfo(data.data))
 }
-phoneDetails()
-const showPhoneDetails = phoneId=>{
+displayDetails()
+const displayInfo = phoneId =>{
     console.log(phoneId)
-    const displayDetails = document.getElementById('details-result')
-    const creatDiv = document.createElement('div')
+    const displayInfo = document.getElementById('display-details')
+    displayInfo.textContent = ''
+    const div = document.createElement('div')
+    div.className = 'col'
+    div.innerHTML = `
+    <div class="card h-100">
+        <img src="${phoneId.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h4 class="card-title">Phone name: ${phoneId.name}</h4>
+          <h6>Brand: ${phoneId.brand}</h6>
+        </div>
+      </div>
+    `
+    displayInfo.appendChild(div)
 }
